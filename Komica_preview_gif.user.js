32051cfdccbb7b9f410a497171245de3e11e0960
usausausausak <usausausausak@gmail.com>
2017-04-06 12:27:16 +0800

    New script: Komica_preview_gif

---
+++ Komica_preview_gif.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         Komica preview gif
+// @namespace    https://github.com/usausausausak
+// @description  Play gif files inplace on komica
+// @include      http://*.komica.org/*/*
+// @include      https://*.komica.org/*/*
+// @version      0.1
+// @grant        none
+// ==/UserScript==
+(function (window) {
+    function playGif(post) {
+        let img = post.querySelector("img");
+        if (!img) {
+            return;
+        }
+
+        let p = img.parentNode;
+        if ((p) && (p.nodeName == "A") && (p.href.match(/\.gif$/))) {
+            img.src = p.href;
+        }
+    }
+
+    document.querySelectorAll(".post").forEach(playGif);
+
+    // observe thread expand
+    let threadObserver = new MutationObserver(function (records) {
+        let postReplys = records.reduce((total, record) => {
+            for (let node of record.addedNodes) {
+                if ((node.classList) &&
+                    (node.classList.contains("reply"))) {
+                    total.push(node);
+                }
+            }
+            return total;
+        } , []);
+        let replySize = postReplys.length;
+        console.log(`[Komica_preview_gif] Reply size change: ${replySize}`);
+
+        postReplys.forEach(playGif);
+    });
+
+    document.querySelectorAll(".thread").forEach(thread => {
+        threadObserver.observe(thread, { childList: true });
+    });
+})(window);
+
