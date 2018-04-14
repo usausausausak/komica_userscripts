3033dc33ae7d2cc388f53220690ea14c765756af
usausausausak <usausausausak@gmail.com>
2018-04-14 18:40:37 +0800

    New library: komica_host_matcher.js

---
+++ libs/komica_host_matcher.js
@@ -0,0 +1,56 @@
+/*
+ * @name         Komica Host Matcher
+ * @description  Check we are at board of which host
+ * @namespace    https://github.com/usausausausak
+ * @version      0.1.0
+ *
+ * Supported host identitys:
+ *   komica.org  => komica
+ *   komica2.net => komica
+ *   2cat.cf     => 2cat
+ *
+ * // Returns the host id, or returns null while the `location` isn't a supported host.
+ * function Komica.hostMatcher(location: Location) -> (nullable)String
+ *
+ * // Returns the host id, or returns the provided `err` while the `location` isn't a supported host.
+ * function Komica.hostMatcherOr(location: Location, err: String) -> String
+ */
+
+if (typeof Komica === 'undefined') {
+  this.Komica = {};
+}
+
+(function komicaPostQueryer(Komica) {
+  'use strict'
+
+  const MATCHER = {
+      'komica': /^([^\.]*)\.komica2?\.(org|net)$/,
+      '2cat':   /^2cat\.cf$/,
+  };
+
+  function hostMatcher(location) {
+    const host = location.host.replace(/:\d+$/, '');
+    for (let [name, matcher] of Object.entries(MATCHER)) {
+        if (matcher.test(host)) {
+            return name;
+        }
+    }
+    return null;
+  }
+
+  function hostMatcherOr(location, err) {
+    const host = hostMatcher(location);
+    return (host) ? host : err;
+  }
+
+  if (typeof Komica === 'object') {
+    Object.entries({
+      'hostMatcher':   hostMatcher,
+      'hostMatcherOr': hostMatcherOr,
+    }).forEach(([key, fn]) => {
+      if (Komica[key] === undefined) {
+        Komica[key] = fn;
+      }
+    });
+  }
+})(this.Komica);
